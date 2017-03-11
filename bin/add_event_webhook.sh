5ff48ef3c5c3161c1990e5414fb707c683b24d38
mironal <miro.gunp@gmail.com>
2017-03-11 16:27:54 +0900

    add_event_webhook.sh に pull_request_review を追加

--- bin/add_event_webhook.sh
+++ bin/add_event_webhook.sh
@@ -10,5 +10,5 @@ check_repo_name
 # PATCH /orgs/:org/hooks/:id
 curl -v -X PATCH -H "Authorization: token ${BOTHUB_GITHUB_TOKEN}" -H "Content-Type: application/json" \
     https://api.github.com/repos/${BOTHUB_USER_NAME}/${BOTHUB_REPO_NAME}/hooks/${BOTHUB_HOOK_ID} \
-    -d '{"events":["issue_comment", "issues", "pull_request_review_comment", "pull_request", "push", "status"]}' \
+    -d '{"events":["issue_comment", "issues", "pull_request_review_comment", "pull_request", "pull_request_review", "push", "status"]}' \
     | jq "."
